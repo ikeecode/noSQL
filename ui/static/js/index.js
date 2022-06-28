e36f3d1738bca69a298c5014ba6369fc5954b1d1
ikeecode <ikeecode@gmail.com>
2022-06-28 00:43:40 +0000

    animation on login page

--- ui/static/js/index.js
+++ ui/static/js/index.js
@@ -3,6 +3,8 @@ const password = document.querySelector('#password')
 const form     = document.querySelector('form')
 const url_connexion = "http://127.0.0.1:5000/connexion"
 const message  = document.querySelector('#message')
+// const fantasy = document.querySelector('#fantasy')
+
 
 window.onload = async (e)=>{
 }
@@ -37,3 +39,83 @@ form.onsubmit = async (e)=>{
     if (response.profil == 'admin') window.location.href = 'admin.html'
   }
 }
+
+
+function randomData(){
+  data = new Array()
+  for(let i=1; i<101; i++){
+    data.push(Math.ceil(Math.random() * 100))
+  }
+  return data
+}
+
+// Animation with d3js
+
+var colorScale = d3.scaleLinear()
+      .domain([0, 100])
+      .range(['yellow', 'red'])
+
+
+let svg = d3.select('#fantasy')
+          .append('svg')
+          .attr('position', 'relative')
+          .attr('top','0')
+          .attr('width', '100%')
+          .attr('height', '100vh')
+
+
+let circles = svg.selectAll('circle')
+          .data(randomData())
+          .enter()
+            .append('circle')
+            .attr('cy', function(value, ind){
+              return Math.random() * value * ind
+            })
+            .attr('cx', function(value, ind){
+              return Math.random() * value * ind
+            })
+            .attr('r', function(value, ind){
+              return Math.random() * value * ind
+            })
+            .attr('fill', function(value){
+              return colorScale(value)
+            })
+            .attr('opacity', '0.6')
+
+function mouve(){
+  circles.transition()
+    .duration(2000)
+    .attr('r', function(value, i){
+        return value * Math.random() * 2
+      })
+      .attr('cy', function(value, i){
+        return Math.random() * value * i
+      })
+      .attr('cx', function(value, i){
+        return Math.random() * value * i
+      })
+      .attr('opacity', function(value){
+        return value * 0.006
+      })
+      .attr('fill', function(value){
+        return colorScale(value * Math.random())
+      })
+}
+
+setInterval(mouve, 2000)
+
+
+document.querySelectorAll('circle').forEach((item, i) => {
+  item.addEventListener('mouseover', (e)=> {
+    console.log('this')
+
+    line = d3.select(e.target)
+            .append('rect')
+            .attr('fill', 'black')
+            .attr('x1', Math.random() * 10)
+            .attr('x2', Math.random() * 10)
+            .attr('y1', Math.random() * 10)
+            .attr('y2', Math.random() * 10)
+
+  })
+})
--- ui/static/js/user.js
+++ ui/static/js/user.js
@@ -5,5 +5,16 @@ const infouserform = document.querySelector('#infouserform')
 
 infouser.addEventListener('click', (e)=>{
   infouserform.classList.toggle('cache')
+  inputs = infouserform.querySelectorAll('input')
+  if (!infouserform.classList.contains('cache')) {
+    sexe = infouserform.querySelector('select')
+    sexe_value = sexe.options[sexe.selectedIndex].value
+    console.log(sexe_value)
+    inputs.forEach((item, i) => {
+      console.log(item.name)
+    })
+
+  }
+
 })
 console.log(user)
